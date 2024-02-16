21d6608c5bca555d87c89efcdc6cac4835735f1b
Matej Vesel <matej.vesel@student.ucll.be>
2024-02-16 16:10:59 +0100

    Completed two more from 01-basic-python.

--- 01-basic-python/04-lists/13-rotate/student.py
+++ 01-basic-python/04-lists/13-rotate/student.py
@@ -1,11 +1,16 @@
-def rotate(xs, n):
-    xs = xs[n:] + xs[:n]
+# def rotate(xs, n):
+#     xs = xs[n:] + xs[:n]
 
-# xs = [1, 2, 3, 4, 5]
-# rotate(xs, 2)
-# print([3, 4, 5, 1, 2], xs)
-# xs = [1, 2, 3, 4, 5]
-# rotate(xs, 1)
-# print([2, 3, 4, 5, 1], xs)
+def rotate(xs, n):
+    for i in range(n):
+        xs.append(xs[0])
+        xs.pop(0)
+
+xs = [1, 2, 3, 4, 5]
+rotate(xs, 2)
+print([3, 4, 5, 1, 2], xs)
+xs = [1, 2, 3, 4, 5]
+rotate(xs, 1)
+print([2, 3, 4, 5, 1], xs)
 
 # Q&A This code does nothing? Why?
--- 01-basic-python/07-sets/02-remove-duplicates/student.py
+++ 01-basic-python/07-sets/02-remove-duplicates/student.py
@@ -1,25 +1,16 @@
-# def remove_duplicates(xs):
-#     xs_copy = xs.copy()
-#     for i in range(len(xs_copy)):
-#         for j in range(len(xs_copy) -1, i, -1):
-#             if xs_copy[i] == xs_copy[j]:
-#                 xs_copy.pop(j)
-#     return xs_copy
-
-# def remove_duplicates(xs):
-#     xs_copy = xs.copy()
-#     for i in range(len(xs_copy) - 1, -1, -1):
-#         if xs_copy[i] in set(xs_copy[i-1:-1:-1]):
-#             xs_copy.pop(i)
-#     return xs_copy
+def remove_duplicates(xs):
+    xs_copy = []
+    xs_set = set(xs)
+    for item in xs:
+        if item in xs_set:
+            xs_copy.append(item)
+            xs_set.remove(item)
+    return xs_copy
 
 
-print([4, 3, 2, 1, 5], remove_duplicates([4, 3, 4, 2, 2, 1, 5]))
+# print([4, 3, 2, 1, 5], remove_duplicates([4, 3, 4, 2, 2, 1, 5]))
 
+# [4, 3, 4, 2, 2, 1, 5]
 # [4, 3, 4, 2, 2, 1, 5]
 # (1, 2, 3, 4, 5)
 # [4, 3, 2, 1, 5]
-
-# [4, 3, 4, 2, 2, 1, 5]
-# [4, 3, 4, 2, 2, 1, 5]
-# (1, 2, 3, 4, 5)
